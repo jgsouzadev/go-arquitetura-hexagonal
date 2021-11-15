34453987d313db59a658c343a449fe791a69fa32
Joao Guilherme de Souza <dev.j@burgerking.com.br>
2021-11-15 20:42:56 -0300

    fix dockerfile e add go test

---
+++ app/product_test.go
@@ -0,0 +1,23 @@
+package app_test
+
+import (
+	"testing"
+
+	"github.com/jgsouzadev/go-arquitetura-hexagonal/app"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := app.Product{}
+	product.Name = "Hello"
+	product.Status = app.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+	require.Nil(t, err)
+
+	product.Price = 0
+	err = product.Enable()
+	require.Equal(t, "The price must be greater than zero to enable the product", err.Error())
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/jgsouzadev/go-arquitetura-hexagonal
 
 go 1.17
+
+require(
+		github.com/stretchr/testify v1.7.0
+)
