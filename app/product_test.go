9d761540777b8103bc81098e3f54fe0c14cb3f1d
Joao Guilherme de Souza <dev.j@burgerking.com.br>
2021-11-15 21:03:10 -0300

    feat: add tests

--- app/product_test.go
+++ app/product_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/jgsouzadev/go-arquitetura-hexagonal/app"
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 )
 
@@ -20,4 +21,42 @@ func TestProduct_Enable(t *testing.T) {
 	err = product.Enable()
 	require.Equal(t, "The price must be greater than zero to enable the product", err.Error())
 
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := app.Product{}
+	product.Name = "Hello"
+	product.Status = app.DISABLED
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+
+	product.Price = 10
+	err = product.Disable()
+
+	require.Equal(t, "the price must be zero in order to have the product disabled", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := app.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "hello"
+	product.Status = app.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "the status must be enabled or disabled", err.Error())
+
+	product.Status = app.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "the price must be greather or equal zero", err.Error())
 }
